c985243b1576e476086061150bddc961315c6942
easn <986260868@qq.com>
2019-05-07 17:57:34 +0800

    SolrUtil中的主方法错了，改了一下

---
+++ pinyougou-solr-until/src/main/java/com/pinyougou/solrutil/SolrUtil.java
@@ -0,0 +1,48 @@
+package com.pinyougou.solrutil;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import com.pinyougou.mapper.TbItemMapper;
+import com.pinyougou.pojo.TbItem;
+import com.pinyougou.pojo.TbItemExample;
+import com.pinyougou.pojo.TbItemExample.Criteria;
+
+/**
+ * 实现商品数据的查询(已审核商品)
+ * @author ASUS
+ *
+ */
+@Component
+public class SolrUtil {
+	@Autowired
+	private TbItemMapper itemMapper;
+	
+	/**
+	 * 导入数据
+	 */
+	public void importItemData(){
+		
+		TbItemExample example=new TbItemExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andStatusEqualTo("1");
+		List<TbItem> items = itemMapper.selectByExample(example);
+		System.out.println("开始");
+		for(TbItem item:items){
+			System.out.println(item.getTitle());
+		}
+		System.out.println("结束");
+		
+	}
+
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("classpath*:spring/applicationContext*.xml");
+		SolrUtil solrUtil= (SolrUtil) context.getBean("solrUtil");
+		solrUtil.importItemData();
+	}
+
+}

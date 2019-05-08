a623398ebba468eec3df1775800e18b62c5cc8a6
easn <986260868@qq.com>
2019-05-08 21:46:48 +0800

    完成搜索的pinyougou-search-service接口层和服务层和控制层完毕

--- pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-@Service
+@Service(timeout = 5000)
 public class ItemSearchServiceImpl implements ItemSearchService {
 
     @Autowired
